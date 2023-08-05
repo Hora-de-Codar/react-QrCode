7860c5704e821ef7cf7aff96c744a8c5d293dfd8
leoHcodar <suporte@horadecodar.com.br>
2023-08-05 02:42:49 -0300

    📚docs: Installing html5Qrcode lib and improve css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,67 @@
-// import { useState } from 'react'
-
-import './App.css'
+import { useState, useEffect } from "react";
+import { Html5Qrcode } from "html5-qrcode";
+import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [isEnabled, setEnabled] = useState(false);
+  const [qrMessage, setQrMessage] = useState("");
 
+  useEffect(() => {
+    const config = { fps: 10, qrbox: { width: 200, height: 200 } };
+    const html5QrCode = new Html5Qrcode("qrCodeContainer");
+
+    const qrScanerStop = () => {
+      if (html5QrCode && html5QrCode.isScanning) {
+        html5QrCode
+          .stop()
+          .then(() => console.log("Scaner stoped"))
+          .catch(() => console.log("Scaner stoped with error"));
+      }
+    };
+
+    const qrCodeSuccess = (decodedText) => {
+      setQrMessage(decodedText);
+      setEnabled(false);
+    };
+
+    if (isEnabled) {
+      html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccess);
+      setQrMessage("");
+    } else {
+      qrScanerStop();
+    }
+    return () => {
+      qrScanerStop();
+    };
+  }, [isEnabled]);
   return (
-    <>
-      <div>
-        <h1 className='bg-blue-400'>Reset React-app</h1>
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div
+        className="relative w-72 h-72 rounded-md overflow-hidden"
+        id="qrCodeContainer"
+      ></div>
+      {qrMessage && (
+        <div className="qr-message absolute top-0 left-0 w-full h-full p-20 flex justify-center items-center bg-blue-400 text-white text-center">
+          {qrMessage}
+        </div>
+      )}
+      <button
+        className="absolute top-10 right-10 p-3 flex rounded-md bg-blue-200 hover:bg-blue-100 transition-all"
+        onClick={() => setEnabled(!isEnabled ? "On" : "Off")}
+      >
+        <span className="material-symbols-outlined">qr_code</span>
+      </button>
+      <div className="absolute top-10 left-10 flex items-center">
+        <img
+          src="src/assets/hora-de-codar.png"
+          alt="Hora de codar"
+          className="w-12 mx-2"
+        />
+        <p className="font-bold text-white">Hoda de codar</p>
       </div>
-    </>
-  )
+
+    </div>
+  );
 }
 
-export default App
+export default App;
