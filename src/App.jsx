bb28419f54a2215d623e2247d1e8b575b7ef484e
Matheus Battisti <matheusbudkewicz@gmail.com>
2023-08-08 17:20:10 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import { Html5Qrcode } from "html5-qrcode";
 import "./App.css";
 
 function App() {
-  const [isEnabled, setEnabled] = useState(false);
+  const [isEnabled, setIsEnabled] = useState(false);
   const [qrMessage, setQrMessage] = useState("");
-  const [isQrCodeVisible, setQrCodeVisible] = useState(false);
 
   useEffect(() => {
     const config = { fps: 10, qrbox: { width: 200, height: 200 } };
@@ -31,21 +30,34 @@ function App() {
     } else {
       qrScanerStop();
     }
+
     return () => {
       qrScanerStop();
     };
   }, [isEnabled]);
 
   const toggleQrCodeVisibility = () => {
-    setQrCodeVisible(!isQrCodeVisible);
+    setIsEnabled(!isEnabled);
   };
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gradient-to-r from-sky-500 to-indigo-500">
+    <div className="fixed top-0 left-0 w-full h-full flex flex-col justify-around items-center bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div className="flex justify-around w-full">
+        <div className="flex items-center">
+          <p className="font-bold text-white">
+            Hora de Codar - Leitor de QR Code
+          </p>
+        </div>
+        <button
+          className="p-3 flex rounded-md bg-blue-200 hover:bg-blue-100 transition-all"
+          onClick={() => toggleQrCodeVisibility()}
+        >
+          <span className="material-symbols-outlined">qr_code</span>
+        </button>
+      </div>
+
       <div
-        className={`relative w-72 h-72 rounded-md overflow-hidden ${
-          isQrCodeVisible ? "block" : "hidden"
-        }`}
+        className={`relative w-72 h-72 rounded-md overflow-hidden`}
         id="qrCodeContainer"
       ></div>
       {qrMessage && (
@@ -53,23 +65,6 @@ function App() {
           {qrMessage}
         </div>
       )}
-      <button
-        className="absolute top-10 right-10 p-3 flex rounded-md bg-blue-200 hover:bg-blue-100 transition-all"
-        onClick={() => {
-          setEnabled(!isEnabled);
-          toggleQrCodeVisibility();
-        }}
-      >
-        <span className="material-symbols-outlined">qr_code</span>
-      </button>
-      <div className="absolute top-10 left-10 flex items-center">
-        <img
-          src="src/assets/hora-de-codar.png"
-          alt="Hora de codar"
-          className="w-12 mx-2"
-        />
-        <p className="font-bold text-white">Hora de codar</p>
-      </div>
     </div>
   );
 }
